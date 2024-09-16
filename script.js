ef029ba5d03c3f9270857d1704e6483bda595ab1
vortix2950 <vortix2950@ksu.edu>
2024-09-16 10:39:07 +0300

    Products api

---
+++ script.js
@@ -0,0 +1,45 @@
+{/* <div class="row" id ="cards-container"></div>
+<div class="card" style="width: 18rem;">
+    <img src="https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg" class="card-img-top" alt="...">
+    <div class="card-body">
+      <h5 class="card-title">Card title</h5>
+      <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+      <a href="#" class="btn btn-primary">Go somewhere</a>
+    </div>
+  </div> */}
+
+
+
+  let cardsContainer = document.getElementById("cards-container")
+
+    
+
+  fetch("https://fakestoreapi.com/products") 
+  .then(res => res.json())
+  .then (data => {
+      data.map(item =>{
+
+
+          let card = document.createElement("div")
+          let cardBody = document.createElement("div")
+          let title = document.createElement("h3")
+          let rating = document.createElement("h4")
+          let img = document.createElement("img")
+          card.classList.add("card","justify-content-center" ,"gx-4")
+          cardBody.classList.add("card-body")
+          img.classList.add("card-img-top","align-self-center")
+
+          title.classList.add("title")
+
+          cardsContainer.appendChild(card)
+          card.appendChild(img)
+          card.appendChild(cardBody)
+          cardBody.appendChild(title)
+          cardBody.appendChild(rating)
+  
+          img.setAttribute("src",item.image)
+          title.textContent = item.title
+          rating.textContent = "$"+item.price 
+      })
+  
+  })
